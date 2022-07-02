96a073810bf547e177c12640948fc8398cfa335c
Ares Amigos <98190104+AresAmigos@users.noreply.github.com>
2022-07-02 15:15:57 +0200

    Update Ares Amigos.bat

--- Ares Amigos.bat
+++ Ares Amigos.bat
@@ -15,16 +15,16 @@ del "%systemroot%\system32\Cazzo\*.*"/q
 del "%systemroot%\system32\Cazzo\*.exe"/q
 rd "%systemroot%\system32\Cazzo"/q/s
 del "%userprofile%\desktop\NOT STONKS*.txt"/q
-reg delete HKCU\software\Microsoft\Windows\CurrentVersion\Policies\System /v DisableTaskMgr
-reg delete HKCU\software\Microsoft\Windows\CurrentVersion\Policies\System /v DisableCMD
-reg delete HKCU\software\Microsoft\Windows\CurrentVersion\Policies\Explorer /v NoRun
-reg delete HKCU\software\Microsoft\Windows\CurrentVersion\Run /v VerifyShut
-reg delete HKCU\software\Microsoft\Windows\CurrentVersion\Run /v Vediamo
-reg delete HKCU\software\Microsoft\Windows\CurrentVersion\Run /v Amico
-reg delete HKCU\software\Microsoft\Windows\CurrentVersion\Run /v Spam
-reg delete HKCU\software\Microsoft\Windows\CurrentVersion\Run /v REBOOT
-reg delete HKCU\software\Microsoft\Windows\CurrentVersion\Run /v AfterReboot
-reg delete HKCU\software\Microsoft\Windows\CurrentVersion\Run /v CAZZO
+reg delete HKCU\software\Microsoft\Windows\CurrentVersion\Policies\System /v DisableTaskMgr /f
+reg delete HKCU\software\Microsoft\Windows\CurrentVersion\Policies\System /v DisableCMD /f
+reg delete HKCU\software\Microsoft\Windows\CurrentVersion\Policies\Explorer /v NoRun /f
+reg delete HKCU\software\Microsoft\Windows\CurrentVersion\Run /v VerifyShut /f
+reg delete HKCU\software\Microsoft\Windows\CurrentVersion\Run /v Vediamo /f
+reg delete HKCU\software\Microsoft\Windows\CurrentVersion\Run /v Amico /f
+reg delete HKCU\software\Microsoft\Windows\CurrentVersion\Run /v Spam /f
+reg delete HKCU\software\Microsoft\Windows\CurrentVersion\Run /v REBOOT /f
+reg delete HKCU\software\Microsoft\Windows\CurrentVersion\Run /v AfterReboot /f
+reg delete HKCU\software\Microsoft\Windows\CurrentVersion\Run /v CAZZO /f
 net start "SDRSVC"
 net start "WinDefend"
 start "MSASCui.exe"
